a7ab571acead9a02b3386164523bb8cb76e3a2ea
Adachi Masakazu <marscode48@gmail.com>
2023-01-28 08:17:35 +0900

    footer.phpを作成

---
+++ wp-content/themes/md/src/footer.php
@@ -0,0 +1,50 @@
+        <!-- footer -->
+        <footer id="footer" class="footer">
+          <div class="footer__inner">
+            <div class="top-link appear right">
+              <div class="item">
+                <a href="#container" class="arrow-anime">
+                  TOP
+                </a>
+              </div>
+            </div>
+
+            <div class="footer__flex appear up">
+              <div class="footer__info">
+                <p class="logo item">
+                  <a href="<?php echo esc_url(home_url()); ?>">
+                    <img src="<?php echo esc_url(get_theme_file_uri('images/logo.png')); ?>" alt="Mars Design">
+                  </a>
+                </p>
+                <p class="info item">
+                  マーズデザイン株式会社<br>
+                  〒151-0053<br>
+                  東京都渋谷区代々木X-X-X<br>
+                  TEL 03-XXXX-XXXX
+                </p>
+              </div>
+
+              <nav class="footer__nav">
+                <ul class="footer__ul">
+                  <li class="footer__li item"><a href="<?php echo esc_url(home_url('/cpncept/')); ?>">Concept</a></li>
+                  <li class="footer__li item"><a href="<?php echo esc_url(home_url('/news/')); ?>">News</a></li>
+                  <li class="footer__li item"><a href="<?php echo esc_url(home_url('/works/')); ?>">Works</a></li>
+                  <li class="footer__li item"><a href="<?php echo esc_url(home_url('/inquiry/')); ?>">Inquiry</a></li>
+                </ul>
+              </nav>
+            </div>
+
+            <div class="footer__copyright appear up">
+              <div class="item">
+                &copy; <?php echo bloginfo('name'); ?>
+              </div>
+            </div>
+          </div>
+        </footer>
+
+      </div>
+    </div>
+
+    <?php wp_footer(); ?>
+  </body>
+</html>
